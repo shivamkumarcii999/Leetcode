0d215f9602b5d6886a42e043ddc196d7d12f0b13
ShivamGupta01032004 <shivamkumarcii999@gmail.com>
2025-11-29 18:07:59 +0530

    Time: 35 ms (48.27%) | Memory: 99.5 MB (7.81%) - LeetSync

---
+++ 2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int partitionArray(int[] nums, int k) {
+        Arrays.sort(nums);
+
+        int count = 1;  // At least one subsequence needed
+        int start = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] - start > k) {
+                count++;
+                start = nums[i];  // Reset starting value for new subsequence
+            }
+        }
+
+        return count;
+    }
+}
